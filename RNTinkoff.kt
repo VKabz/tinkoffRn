68bb76acb28a6398375b05c409344479272b73d4
Ivan Kobizkiy <39455342+VKabz@users.noreply.github.com>
2024-02-29 20:22:46 +0300

    Create RNTinkoff.kt

---
+++ RNTinkoff.kt
@@ -0,0 +1,13 @@
+package com.reactnativeasdktinkoff
+
+import android.app.Activity
+
+public class RNTinkoff {
+  companion object {
+    fun init(activity: Activity) {
+      AsdkTinkoffModule.init(activity)
+    }
+  }
+
+
+}
