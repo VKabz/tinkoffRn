30ff5ef409674b3184c57a16c392b2266883f8b6
Ivan Kobizkiy <39455342+VKabz@users.noreply.github.com>
2024-02-29 20:22:14 +0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,55 @@
+package com.vkabzdev.taxi_driver_app;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+// Add these required imports:
+import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash;
+import com.reactnativeasdktinkoff.RNTinkoff;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "TaxiDriverAppReBuild";
+  }
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this); // <- initialize the splash screen
+
+
+
+    // ТУТ ДЕЛАЮ ПЕРЕДАЧУ ЧТОБЫ registerForActivityResult ДО ЗАПУСКА ПРИЛОЖЕНИЯ
+    RNTinkoff.Companion.init(this);
+
+
+
+    super.onCreate(null); // or super.onCreate(null) with react-native-screens
+  }
+
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * (aka React 18) with two boolean flags.
+   */
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
+        // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
+        DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
+        );
+  }
+}
